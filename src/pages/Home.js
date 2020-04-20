82195ff9676b3630d637634dc93762763022bd78
masendhy <massendhy@gmail.com>
2020-04-20 21:54:37 +0700

    Home and Rooms page added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,13 @@ import Banner from '../component/Banner';
 import {Link} from 'react-router-dom'
 
 export default function Home() {
-    return <Hero>
+    return (
+    <Hero>
         <Banner title='luxurious rooms' subtitle='deluxe rooms starting at IDR 3 Million'>
     <Link to="/rooms" className="btn-primary">
         our rooms
         </Link>
         </Banner>
     </Hero>
+    )
 }
