5846666320f76cc3b4b144662e09f4969c6b4e38
masendhy <massendhy@gmail.com>
2020-04-22 21:54:36 +0700

    App,services and Home page adited

--- src/component/Services.js
+++ src/component/Services.js
@@ -1,13 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import{FaCocktail,FaHiking,FaBeer, FaShuttleVan} from 'react-icons/fa';
 import Title from './Title'
 
 
 export default class Services extends Component {
+    state={
+        services:[
+            {
+                icon:<FaCocktail/>,
+                title:'free coktails',
+                info:'Lorem Ipsum is not simply random text',
+            },
+            {
+                icon:<FaHiking/>,
+                title:'Endless Hiking',
+                info:'Lorem Ipsum is not simply random text',
+            },
+            {
+                icon:<FaShuttleVan/>,
+                title:'free Shuttle',
+                info:'Lorem Ipsum is not simply random text',
+            },
+            {
+                icon:<FaBeer/>,
+                title:'Strongest Beer',
+                info:'Lorem Ipsum is not simply random text',
+            }
+
+        ]
+    }
     render() {
         return (
-            <div>
-                hello from services
-            </div>
+            <section className="services">
+                <Title title="services"/>
+                <div className="services-center">
+                    {this.state.services.map((item,index)=>{
+                        return<article key={index} className="service">
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                        </article>
+                    })}
+
+                </div>
+                
+            </section>
         )
     }
 }
