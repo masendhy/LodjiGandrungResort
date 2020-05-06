5194f51bbd5dc39775b5d2afeaa247d541223852
masendhy <massendhy@gmail.com>
2020-05-06 21:53:31 +0700

    features 02.52

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,26 @@
-import React from 'react'
-import Hero from '../component/Hero';
-import Banner from '../component/Banner';
-import {Link} from 'react-router-dom';
-import Services from '../component/Services';
-import FeaturedRooms from '../component/FeaturedRooms';
-
-
-export default function Home() {
-    return (
-        <>
-    <Hero>
-        <Banner title='luxurious rooms' subtitle='deluxe rooms starting at IDR 3 Million'>
-    <Link to="/rooms" className="btn-primary">
-        our rooms
-        </Link>
+import React from "react";
+import { Link } from "react-router-dom";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import Services from "../components/Services";
+import FeaturedRooms from "../components/FeaturedRooms";
+const home = () => {
+  return (
+    <>
+      <Hero>
+        <Banner
+          title="luxurious rooms"
+          subtitle="deluxe rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
         </Banner>
-    </Hero>
-    <Services/>
-    <FeaturedRooms/>
+      </Hero>
+      <Services />
+      <FeaturedRooms />
     </>
-    )
-}
+  );
+};
+
+export default home;
